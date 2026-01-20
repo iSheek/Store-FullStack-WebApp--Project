eb5aaf2c69fa29ad20dd3e50113de05ed593288e
48604 <igor.szyk@outlook.com>
2026-01-21 00:14:06 +0100

    initial commit

---
+++ init.sql
@@ -0,0 +1,21 @@
+create table if not exists categories(
+	id int not null primary key,
+	name varchar(30)
+);
+
+create table if not exists products_images(
+	id int not null primary key,
+	image_path varchar(50)
+);
+
+create table if not exists products(
+	id int not null primary key,
+	name varchar(50),
+	category_id int,
+	image_id int,
+	foreign key (image_id) references products_images(id),
+	foreign key (category_id) references categories(id)
+);
+
+
+
