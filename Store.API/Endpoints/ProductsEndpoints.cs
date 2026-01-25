554e8495ab0f5cd9f02e72661a270025c2839363
48604 <igor.szyk@outlook.com>
2026-01-25 01:31:23 +0100

    seperate endpoints from Program.cs

---
+++ Store.API/Endpoints/ProductsEndpoints.cs
@@ -0,0 +1,77 @@
+using System;
+using Store.API.Data;
+using Store.API.DTO;
+using Store.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Store.API.Endpoints;
+
+public static class ProductsEndpoints
+{   
+    const string GetProductEndpoint = "GetProduct";
+    public static void MapProductsEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/products");
+
+        group.MapGet("/", async (StoreContext dbContext) =>
+            await dbContext.Products
+                            .Include(product => product.Category)
+                            .Include(product => product.ProductImage)
+                            .Select(product => new ProductDTO(
+                                product.Id,
+                                product.Name,
+                                product.Price,
+                                product.Category!.Name,
+                                product.ProductImage!.ImagePath
+                            ))
+                            .AsNoTracking()
+                            .ToListAsync());
+
+
+        group.MapGet("/{id}", async (int id, StoreContext dbContext) =>
+        {
+
+            var product = await dbContext.Products.FindAsync(id);
+
+            return product is null ? Results.NotFound() : Results.Ok(
+                new ProductDTO(
+                    product.Id,
+                    product.Name,
+                    product.Price,
+                    product.Category!.Name,
+                    product.ProductImage!.ImagePath
+                )
+            );
+
+        })
+            .WithName(GetProductEndpoint);
+
+        group.MapPost("/", async (CreateProductDTO newProduct, StoreContext dbContext) =>
+        {
+            Product product = new()
+            {
+                Name = newProduct.Name,
+                Price = newProduct.Price,
+                CategoryId = newProduct.CategoryId,
+                ProductImageId = newProduct.ImagePathId
+            };
+            
+            dbContext.Products.Add(product);
+
+            await dbContext.SaveChangesAsync();
+
+            ProductInfoDTO productInfo = new(
+                product.Id,
+                product.Name,
+                product.Price,
+                product.CategoryId,
+                product.ProductImageId
+            );
+
+            return Results.CreatedAtRoute(GetProductEndpoint, new {id = productInfo.Id}, productInfo);
+
+        });
+
+    }
+
+}
--- Store.API/Program.cs
+++ Store.API/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Store.API.Data;
-using Store.API.DTO;
-using Store.API.Models;
+using Store.API.Endpoints;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-const string GetProductEndpoint = "GetProduct";
+
 
 builder.AddStoreDb();
 
@@ -15,65 +15,7 @@ app.MapGet("/", () => "Hello World!");
 
 // app.MapGet("/{id}", () => "");
 
-app.MapGet("/products", async (StoreContext dbContext) =>
-    await dbContext.Products
-                    .Include(product => product.Category)
-                    .Include(product => product.ProductImage)
-                    .Select(product => new ProductDTO(
-                        product.Id,
-                        product.Name,
-                        product.Price,
-                        product.Category!.Name,
-                        product.ProductImage!.ImagePath
-                    ))
-                    .AsNoTracking()
-                    .ToListAsync());
-
-
-app.MapGet("/products/{id}", async (int id, StoreContext dbContext) =>
-{
-
-    var product = await dbContext.Products.FindAsync(id);
-
-    return product is null ? Results.NotFound() : Results.Ok(
-        new ProductDTO(
-            product.Id,
-            product.Name,
-            product.Price,
-            product.Category!.Name,
-            product.ProductImage!.ImagePath
-        )
-    );
-
-})
-    .WithName(GetProductEndpoint);
-
-app.MapPost("/products", async (CreateProductDTO newProduct, StoreContext dbContext) =>
-{
-    Product product = new()
-    {
-        Name = newProduct.Name,
-        Price = newProduct.Price,
-        CategoryId = newProduct.CategoryId,
-        ProductImageId = newProduct.ImagePathId
-    };
-    
-    dbContext.Products.Add(product);
-
-    await dbContext.SaveChangesAsync();
-
-    ProductInfoDTO productInfo = new(
-        product.Id,
-        product.Name,
-        product.Price,
-        product.CategoryId,
-        product.ProductImageId
-    );
-
-    return Results.CreatedAtRoute(GetProductEndpoint, new {id = productInfo.Id}, productInfo);
-
-});
-
+app.MapProductsEndpoints();
 
 
 app.MigrateDb();
